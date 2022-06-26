76ab7da414fb62962f8fe2636530ecd575a8a369
Anton Kuptsov <anton.kuptsou@gmail.com>
2022-06-26 19:42:38 +0400

    add custom styles to Github link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,8 +50,9 @@ module.exports = {
         },
         {
           href: "https://github.com/cloudquery/cloudquery",
-          label: "GitHub",
           position: "right",
+          className: "header-github-link",
+          "aria-label": "GitHub repository",
         },
       ],
     },
